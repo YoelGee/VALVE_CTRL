f9ed64d49f0dc0a8fb984ad8750a30a8c06aa02f
Yoel Ghebrecristos <yghebrec@uci.edu>
2023-08-11 14:06:40 -0700

    La

--- VALVE_CTRL.ino
+++ VALVE_CTRL.ino
@@ -1,15 +1,11 @@
 #include "valve_menu.h"
 ValveMenu v;
 void setup(){
-<<<<<<< HEAD
     //v.StartTest();
-=======
-    Serial.begin(115200);
-    v.StartTest();
->>>>>>> 40244283e5cbd8f83465137fbc10ef7ee8d05c15
+    v.MainMenu();
     
 }
 
 void loop(){
-    v.MainMenu();
+    
 }
